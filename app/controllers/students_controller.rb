2eee36bfced3e6b032733d2b9ebb373317670125
Kazungu Derrick <kazunguderriq@gmail.com>
2022-11-21 14:32:39 +0300

    commit tree 2b36b114c31d750c11eadaf80ff80d6f313eb995

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,11 @@
 class StudentsController < ApplicationController
-    def index
-        students = Student.all
-        render json: students
-    end
+  def index
+    students = Student.all
+    render json: students
+  end
 
+  def grades
+    data = Student.order(grade: :desc)
+    render json: data
+  end
 end
